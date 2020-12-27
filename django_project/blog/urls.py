591dc661c9543cb16d152593240a4d83f9f6e803
Guru Swaroop <bguruswaroop@gmail.com>
2020-12-27 18:52:17 +0100

    Full featured web-app * Use pre-designed styles for better visualization * Add custom CSS Style sheet

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+# It is advisable to use unique names for the paths
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about')
