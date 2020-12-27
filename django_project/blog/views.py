a490e68aefec87b8e11a1b9473c8efe9863c7316
Guru Swaroop <bguruswaroop@gmail.com>
2020-12-27 16:56:22 +0100

    Full featured web-app * Using templates and rendering html pages * Base template and template inheritence * Using bootstrap for CSS

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,29 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Guru Swaroop',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Dec 27, 2020'
+    },
+    {
+        'author': 'Sukhmander Singh',
+        'title': 'Blog Post 2',
+        'content': 'Second blog post',
+        'date_posted': 'Dec 27, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # The keys of the context will be accessible within the template
+    context = {
+        'posts': posts
+    }
+    # The following html page will be searched in the templates sub-directory within the blog app by django
+    return render(request=request, template_name='blog/home.html', context=context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request=request, template_name='blog/about.html', context={'title': 'About'})
